933301b096e6a15468891f478fb97b2229f67f3f
Georgina5-2 <122113060+Georgina5-2@users.noreply.github.com>
2023-05-29 21:21:53 -0400

    Controller files added

---
+++ Controllers/reactionController.js
@@ -0,0 +1,123 @@
+const{User,Thought}=require('../Models');
+
+module.exports={
+    async getThoughts(req,res){
+        try{
+            const thoughtsData=await Thought.find();
+            res.status(200).json(thoughtsData);
+        }
+        catch(err){
+            res.status(500).json(err);
+        }
+    },
+
+    async getSingleThought(req,res){
+        try{
+            const thoughtData=await Thought.findOne({_id:req.params.thoughtId})
+            .select("-__v")
+            .select("-reactions.Id");
+            if(!thoughtData){
+                res.status(400).json({message:`No such thought found`});
+                return;
+            }
+            res.status(200).json(thoughtData);
+        }
+        catch(err){
+            res.status(500).json(err);
+        }
+    },
+
+    async addThought(req,res){
+        try{
+            const thoughtData=await Thought.create(req.body);
+            const userData=await User.findOneAndUpdate(
+                {username:req.body.username},
+                {$addToSet:{thought:thoughtData._id}},
+                {runValidators:true, new:true},
+            )
+            if(!userData){
+                res.status(400).json({message:`No such user found`});
+                return;
+            }
+            res.status(200).json(thoughtData);
+        }
+        catch(err){
+            res.status(500).json(err);
+        }
+    },
+
+    async updateThought(req,res){
+        try{
+            const thoughtData=await Thought.findOneAndUpdate(
+                {_id:req.params.thoughtId},
+                {$Set:req.body},
+                {runValidators:true, new:true}
+            )
+            if(!thoughtData){
+                res.status(400).json({message:`No such thought found!`});
+                return;
+            }
+            res.status(200).json(thoughtData);
+        }
+        catch(err){
+            res.status(500).json(err);
+        }
+    },
+
+    async removeThought(req,res){
+        try{
+            const thoughtData=await Thought.findOneAndDelete(
+                {_id:req.params.thoughtId},
+            )
+            if(!thoughtData){
+                res.status(400).json({message:'No such thought found!'});
+            }
+            const userData=await User.findOneAndUpdate(
+                {_id:thoughtData.userId},
+                {$pull:{thoughts:thoughtData._id}},
+                {new:true},
+            )
+            res.status(200).json({message:`Thought successfully deleted`});
+        }
+        catch(err){
+            res.status(500).json(err);
+        }
+    },
+
+    async addReaction(req,res){
+        try{
+            const thoughtData= await Thought.findOneAndUpdate(
+                {_id:req.params.thoughtId},
+                {$addToSet:{reactions:req.body}},
+                {runValidators:true,new:true},
+            )
+            if(!thoughtData){
+                res.status(400).json({message:`No such thought found!`});
+                return;
+            }
+            res.status(200).json(thoughtData);
+        }
+        catch(err){
+            res.status(500).json(err);
+        }
+    },
+
+    async removeReaction(req,res){
+        try{
+            const thoughtData=await Thought.findOneAndUpdate(
+                {_id:req.params.thoughtId},
+                {$pull:{reactions:{reactionId:req.params.reactionId}}},
+                {runValidators:true, new:true},
+            );
+            if(!thoughtData){
+                res.status(400).json({message:'No such thought found!'});
+                return;
+            }
+            res.status(200).json(thoughtData);
+
+            }
+            catch(err){
+                res.status(500).json(err);
+            }
+        },
+    };
---
+++ Controllers/userController.js
@@ -0,0 +1,134 @@
+const{User,Thought}=require('../Models');
+
+
+module.exports={
+    //get all users
+    async getUsers(req,res){
+        try{
+            const userData=await User.find();
+            return res.json(userData);
+        }
+        catch(err){
+            res.status(500).json.err;
+        }
+    },
+    //get a single user
+    async getSingleUser(req,res){
+        try{
+            const userData=await User.findOne({_id:req.params.userId})
+            .select("-__v")
+            .populate("thoughts")
+            .populate({path:friends,select:"-thoughts"});
+        if(!userData){
+            res.status(400).json({message:`OOPS,No such user found!`});
+            return;        
+        }
+        res.status(200).json(userData);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+},
+
+async createUser(req,res){
+    try{
+        const userData=await User.create(req.body);
+        res.status(200).json(userData);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+},
+
+async updateUser(req,res){
+    try{
+        const userData= await User.findOneAndUpdate(
+            {_id:req.params.userId},
+            {$set:req.body},
+            {runValidators:true,new:true},
+        )
+        if(!userData){
+            res.status(400).json({message:`OOPS,No such user found!`});
+            return;
+        }
+        res.status(200).json({message:`User's info updated!`});
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+
+},
+
+async deleteUser(req,res){
+    try{
+        const userData=await User.findOneAndDelete({_id:req.params.UserId});
+        if(!userData){
+            res.status(400).json({message:`No such user found!`});
+            return;
+        }
+        const thoughtsData= await Thought.deleteMany({$in:userData.username});
+        res.status(200).json({message:`User deleted successfully`});
+    }
+    catch(err)
+    {
+        res.status(400).json(err);
+    }
+},
+
+async addFriend(req,res){
+    try{
+        const userData=await User.findOneAndUpdate(
+            {_id:req.params.userId},
+            {$addToSet:{friends:req.params.friendId}},
+            {runValidators:true, new:true},
+            )
+        if(!userData){
+            res.status(400).json({message:'No such user found!'});
+            return;
+        }
+        const friendData= await User.findOneAndUpdate(
+            {_id:req.params.friendId},
+            {$addToSet:{friends:req.params.userId}},
+            {runValidators:true,new:true},
+            )
+            if(!friendData){
+                res.status(400).json(err);
+                return;
+            }
+            res.status(200).json({message:'New friend has been added!'});
+        }
+        catch(err){
+            res.status(500).json(err);
+        }
+},
+
+async removeFriend(req,res){
+    try{
+        const userData= await User.findOneAndUpdate(
+            {_id:req.params.userId},
+            {$pull:{friends:req.params.friendId}},
+            {runValidators:true,new:true}
+        )
+        if(!userData){
+            res.status(400).json({message:`No such user found`});
+            return;
+        }
+        
+        const friendData=await User.findOneAndUpdate(
+            {_id:req.params.friendId},
+            {$pull:{friends:req.params.userId}},
+            {runValidators:true,new:true},
+        
+        )
+        res.status(200).json('User data deleted successfully!');
+        }
+        catch(err){
+            res.status(500).json(err);
+        }
+}
+};
+
+
+
+
+
